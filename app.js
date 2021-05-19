b979b14b60d2d33ba1ded3fcb36ce16dd087f07e
Hanzla Haroon <hanzla.haroon@outlook.com>
2021-05-20 00:40:31 +0500

    Added orders routes handling

--- app.js
+++ app.js
@@ -4,10 +4,17 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var redis = require('redis');
+var client = redis.createClient();
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+client.on('error', (err) => {
+  console.log('Redis error: ', err);
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -17,18 +24,68 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+client.on('connect', function () {
+  console.log('connected');
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.get('/order/:price', function (req, res, next) {
+  let price = req.params.price;
+  client.get(price, function (err, obj) {
+    if (!obj) {
+      res.statusCode = 404;
+      res.end(`${price} does not exists!`);
+    } else {
+      req.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.json(obj);
+    }
+  });
+});
+
+app.post('/order', function (req, res, next) {
+  let price = req.body.price;
+  let side = req.body.side;
+
+  client.exists(price, function (err, exists) {
+    if (!exists) {
+      client.set(price, side, function (err, reply) {
+        if (reply === 'OK') {
+          req.statusCode = 200;
+          res.setHeader('Content-Type', 'application/json');
+          res.json(reply);
+        }
+      });
+    } else {
+      client.get(price, function (err, obj) {
+        if (obj && obj != side) {
+          client.del(price, function (err, obj) {
+            req.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            // res.json(obj);
+            res.end(`${price} deleted successfully!`);
+          });
+        } else {
+          res.statusCode = 403;
+          res.end(`${price} already exists!`);
+        }
+      });
+    }
+  });
+});
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
